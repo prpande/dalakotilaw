ae3c2220f43c917039c1e3dd6871119567f7da60
Pratyush Pande <pratyush.pande@gmail.com>
2025-05-01 18:53:33 +0530

    update

--- src/app/contact/services/email.service.ts
+++ src/app/contact/services/email.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+declare let Email: any;
 @Injectable({
   providedIn: 'root'
 })
@@ -6,5 +7,16 @@ export class EmailService {
 
   constructor() { }
   sendTestEmail() {
-    }
+    Email.send({
+      Host : "smtp.elasticemail.com",
+      Username : "dalakoti.aditi@outlook.com",
+      Password : "0FA5E198CE93419297ADED1F2624E8257A15",
+      To : 'pratyush.pande@gmail.com',
+      From : "dalakoti.aditi@outlook.com",
+      Subject : "This is the subject",
+      Body : "And this is the body"
+  }).then(
+    (message: any) => console.log(message)
+  );
+  }
 }

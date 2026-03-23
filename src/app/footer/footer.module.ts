c6e4bd77f7c77b3073add354d9a7fbe443aad4db
Pratyush Pande <pratyush.pande@gmail.com>
2026-03-24 01:29:50 +0530

    fix: use translate pipe for all footer text (address, phone, email, copyright, disclaimer)

--- src/app/footer/footer.module.ts
+++ src/app/footer/footer.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
-
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    FooterComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    FooterComponent
-  ]
+  declarations: [FooterComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [FooterComponent]
 })
 export class FooterModule { }
